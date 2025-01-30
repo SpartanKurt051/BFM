6879af89c0cd063b329ace88a0b31faa18c189e1
SpartanKurt051 <gangulysubhradeep@gmail.com>
2025-01-30 10:02:56 +0000

    Added Reliance stock data to dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -1,2 +1,10 @@
 import streamlit as st
 st.write("Hello World")
+
+
+import streamlit as st
+import pandas as pd
+
+st.title('Reliance Industries Stock Data')
+df = pd.read_csv('reliance_stock_data.csv')
+st.dataframe(df)

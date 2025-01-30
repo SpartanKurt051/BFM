199395c4c5d54b7fb9d0f4aeed2f8c83ad6a3d88
SpartanKurt051 <37214287+SpartanKurt051@users.noreply.github.com>
2025-01-30 16:22:16 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,1 +1,15 @@
-!pip install scikit-learn
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+st.write("Hello World")
+
+
+import streamlit as st
+import pandas as pd
+
+st.title('Reliance Industries Stock Data')
+df = pd.read_csv('reliance_stock_data.csv')
+st.dataframe(df)
+

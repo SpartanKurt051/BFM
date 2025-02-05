f7105cc2f563d02db824498c353950f29c5f965f
SpartanKurt051 <37214287+SpartanKurt051@users.noreply.github.com>
2025-02-05 16:46:29 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -7,9 +7,6 @@ from sklearn.metrics import mean_squared_error
 #Create a ticker-dropdown
 
 # Load CSS file
-import streamlit as st
-
-# Load CSS file
 def load_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)

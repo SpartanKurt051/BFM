8af717dd2316812967f783057829194409271fc7
SpartanKurt051 <37214287+SpartanKurt051@users.noreply.github.com>
2025-02-05 11:37:16 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -3,8 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-st.write("Hello World")
-
 
 import streamlit as st
 import pandas as pd

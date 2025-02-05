52b4640ad31992651a71e6064116407ca5a1dcf4
SpartanKurt051 <gangulysubhradeep@gmail.com>
2025-02-05 06:25:09 +0000

    Added Adani.csv and updated dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -12,3 +12,11 @@ df = pd.read_csv('reliance_stock_data.csv')
 st.dataframe(df)
 
 
+
+
+import streamlit as st
+import pandas as pd
+
+st.title('AdaniGreen Stock Data')
+df = pd.read_csv('AdaniGreen_data.csv')
+st.dataframe(df)

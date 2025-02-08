748175ae41bdb47ca45d0e81277117c11a74f19e
SpartanKurt051 <37214287+SpartanKurt051@users.noreply.github.com>
2025-02-09 04:30:11 +0530

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -201,7 +201,7 @@ def plot_buying_decision(company_name, data):
 
 # Main function
 def main():
-    st.title("📈 Stock Market Dashboard")
+    st.markdown("<h1 style='text-align: center; color: goldenrod;'>📈 Stock Market Dashboard</h1>", unsafe_allow_html=True)
     
     # Sidebar
     st.sidebar.header("Select Company")
